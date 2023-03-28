6c9a5b19d2f061192fc01c0202538fed80a0ddf8
ximuisgoddman <72920822+ximuisgoddman@users.noreply.github.com>
2023-03-28 16:30:41 +0800

    Update models.py

--- books/models.py
+++ books/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from users.models import User
+from users.models import MyUser
 
 
 class Book(models.Model):
@@ -11,7 +11,7 @@ class Book(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books')
+    owner = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
         return self.name

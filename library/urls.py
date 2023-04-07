cd587f0ca56d60941048a9bb2f03fa2b8bdc5ef5
ximuisgoddman <72920822+ximuisgoddman@users.noreply.github.com>
2023-04-07 20:46:24 +0800

    Update urls.py

--- library/urls.py
+++ library/urls.py
@@ -19,6 +19,7 @@ import os
 import sys
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import include
+
 book_path = os.path.abspath(os.path.join(os.getcwd(), "..", "books"))
 user_path = os.path.abspath(os.path.join(os.getcwd(), "..", "users"))
 sys.path.append(book_path)
@@ -27,16 +28,19 @@ from books import views as lib_views
 from users import views as user_views
 from borrow_record import views as borrow_record_view
 from django.contrib import admin
+
 urlpatterns = [
     path('books/', lib_views.book_list, name='book_list'),
     path('books/add/', lib_views.book_create, name='book_create'),
     path('books/<int:book_id>/', lib_views.book_detail, name='book_detail'),
-    path('books/<int:book_id>/update/', login_required(lib_views.book_update), name='book_update'),
-    path('books/<int:book_id>/delete/', login_required(lib_views.book_delete), name='book_delete'),
+    path('books/<int:book_id>/update/', lib_views.book_update, name='book_update'),
+    path('books/<int:book_id>/delete/', lib_views.book_delete, name='book_delete'),
+    path('books/<int:book_id>/borrow/', lib_views.book_borrow, name='book_borrow'),
     path("captcha/", include('captcha.urls')),
     path("login/", user_views.my_login, name='login'),
     path("register/", user_views.my_register, name='register'),
     path('logout/', user_views.logout, name='logout'),
-    path('borrow_record/<int:record_id>',borrow_record_view.borrow_record_view,name='borrow_record_view'),
+    path('borrow_record/<int:record_id>/', borrow_record_view.borrow_record, name='borrow_record'),
+    path('borrow_record_detail/<int:record_id>/', borrow_record_view.borrow_record_detail, name='borrow_record_detail'),
     path('admin/', admin.site.urls)
 ]

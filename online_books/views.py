0fef03cdfc9f9345cec7d524ea1da467cef728a1
ximuisgoddman <72920822+ximuisgoddman@users.noreply.github.com>
2023-04-18 20:22:07 +0800

    Update views.py

--- online_books/views.py
+++ online_books/views.py
@@ -21,7 +21,7 @@ def online_book_list(request):
 
 @login_required
 def online_book_create(request):
-    print("request.POST:", request.POST)
+    print("request.POST:", request.POST, request.FILES)
     form = OnlineBooksForm(request.POST, request.FILES)
     if form.is_valid():
         book = form.save(commit=False)
@@ -69,31 +69,20 @@ def add_book_shelf(request, book_id):
         return render(request, 'book_shelf/book_shelf_detail.html', {'bookshelf': bookshelf})
 
     online_book = OnlineBooksModel.objects.filter(id=book_id).first()
-    print("online_book", online_book,type(online_book.book_image))
-    form_datas = {
-        "book_name": online_book.book_name,
-        "book_author": online_book.book_author,
-        "book_publisher": online_book.book_publisher,
-        "book_image": online_book.book_image,
-        "book_id": online_book.id,
-        'book_shelf_user_id': request.user.id
-    }
-    form_data = QueryDict('', mutable=True)
-    form_data.update(form_datas)
-    form = BookShelfForm(form_data, request.FILES)
-    print("Error", form.errors, "form_data:%s" % form_data)
-    if form.is_valid():
-        from django.core.files import File
-        from io import BytesIO
-        bookshelf = form.save(commit=False)
-        # 将 ImageFieldFile 对象转换为 File 对象
-        if bookshelf.book_image:
-            in_memory_file = BytesIO()
-            bookshelf.book_image.save(in_memory_file, bookshelf.book_image.format)
-            bookshelf.book_image = File(in_memory_file, name=bookshelf.book_image.name)
+    print("online_book", online_book, type(online_book.book_image))
+    if online_book:
+        bookshelf = BookShelfModel(
+            book_id=online_book.id,
+            book_name=online_book.book_name,
+            book_author=online_book.book_author,
+            book_publisher=online_book.book_publisher,
+            book_image=online_book.book_image,
+            book_shelf_user_id=request.user
+        )
         bookshelf.save()
+
     bookshelfs = BookShelfModel.objects.all()
     print("Len", len(bookshelfs), request.FILES)
     for x in bookshelfs:
-        print("@@", x.id, x.book_name,x.book_image)
+        print("@@", x.id, x.book_name, x.book_image)
     return render(request, 'book_shelf/book_shelf_list.html', {'bookshelfs': bookshelfs})

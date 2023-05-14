2e7309f9ddab9e01d22c3d483dcc5ab08371c200
ximuisgoddman <72920822+ximuisgoddman@users.noreply.github.com>
2023-05-14 18:18:12 +0800

    Update settings.py

--- library/settings.py
+++ library/settings.py
@@ -83,26 +83,43 @@ WSGI_APPLICATION = 'library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#   'default':
-#      {
-#         'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
-#        'NAME': 'library_manage_system',  # 数据库名称
-#       'HOST': '127.0.0.1',  # 数据库地址，本机 ip 地址 127.0.0.1
-#      'PORT': 3306,  # 端口
-#     'USER': 'root',  # 数据库用户名
-#    'PASSWORD': 'LUO808089lin@',  # 数据库密码
-# }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': '3306',
-    }
+    'default':
+        {
+            'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
+            'NAME': os.getenv('DB_NAME'),  # 数据库名称
+            'HOST': '127.0.0.1',  # 数据库地址，本机 ip 地址 127.0.0.1
+            'PORT': 3306,  # 端口
+            'USER': 'root',  # 数据库用户名
+            'PASSWORD': 'LUO808089lin@',  # 数据库密码
+        }
+}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': '3306',
+#     }
+# }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, "log_files", "log.txt"),
+        },
+    },
+    'loggers': {
+        'myapp': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    },
 }
 
 CACHES = {

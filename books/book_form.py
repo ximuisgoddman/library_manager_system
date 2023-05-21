80532a8a32838dfc5117502712d8f05dee7ec17d
ximuisgoddman <72920822+ximuisgoddman@users.noreply.github.com>
2023-05-21 16:42:31 +0800

    Update book_form.py

--- books/book_form.py
+++ books/book_form.py
@@ -10,11 +10,13 @@ class BookForm(forms.ModelForm):
     ]
     book_classification = forms.ChoiceField(choices=BOOK_CLASS_CHOICES)
     file_upload = forms.FileField(required=False)  # 添加文件上传字段
+
     class Meta:
         model = Book
-        fields = ['book_name', 'author', 'publisher', 'publish_time', 'book_numbers', 'current_number',
+        fields = ['book_image', 'book_name', 'author', 'publisher', 'publish_time', 'book_numbers', 'current_number',
                   'book_classification']
         labels = {
+            'book_image': '图书封面',
             'book_name': '书名:',
             'author': '作者:',
             'publisher': '出版社:',

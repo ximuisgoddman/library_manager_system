d9f9b2862dd72cd478d3ac462f3e6dbe87bc3e27
ximuisgoddman <72920822+ximuisgoddman@users.noreply.github.com>
2023-05-26 19:24:17 +0800

    Update online_song_form.py

--- online_song/online_song_form.py
+++ online_song/online_song_form.py
@@ -3,8 +3,10 @@ from .models import OnlineSongModel
 
 
 class OnlineSongForm(forms.ModelForm):
+    file_upload = forms.FileField(required=False)
     class Meta:
         model = OnlineSongModel
+
         fields = ('song_title', 'audio_file', 'song_duration', 'song_author', 'song_classification')
         labels = {
             'song_title': "歌名",

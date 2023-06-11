921efc9ced30d4051200673123b5d4b8f74a8b74
muzifan1003 <muzifan1003@outlook.com>
2023-06-11 13:45:24 +0800

    增加倒计时

--- my_resource/music/manage_music.py
+++ my_resource/music/manage_music.py
@@ -2,8 +2,8 @@ import os
 from mutagen.mp3 import MP3
 import re
 
-mp3_file = "songs_file"
-
+mp3_file = "media/audio"
+print(os.path.abspath(mp3_file))
 for each_file in os.listdir(mp3_file):
     pattern = r"(.*?)\s-\s(.*?)\s\["
     author, song_name = re.search(pattern, each_file).groups()
@@ -12,6 +12,6 @@ for each_file in os.listdir(mp3_file):
         zhuanji = audio["TALB"]
     except Exception:
         zhuanji = "--"
-    timestamp = int(audio.info.length)
+    timestamp = "%02d:%02d" %(int(audio.info.length)//60,int(audio.info.length)%60)
     with open("my_music.txt", 'a', encoding='utf-8') as fw:
         fw.write("%s,%s,%s,%s,%s\n" % (each_file, author, song_name, zhuanji, timestamp))

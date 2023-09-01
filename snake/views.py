a23663575a3e31bce8b7136ce89b118989b6b409
muzifan1003 <muzifan1003@outlook.com>
2023-09-01 13:10:51 +0800

    modify snake

--- snake/views.py
+++ snake/views.py
@@ -23,19 +23,21 @@ def new_food(n):
 
     # 在游戏初始化时，生成5个食物点的位置
     for _ in range(n):
-        food_x = round(random.randrange(0, WINDOW_WIDTH - snake_size) / 10.0) * 10.0
-        food_y = round(random.randrange(0, WINDOW_HEIGHT - snake_size) / 10.0) * 10.0
+        food_x = round(random.randrange(0, WINDOW_WIDTH - snake_size) / 10) * 10
+        food_y = round(random.randrange(0, WINDOW_HEIGHT - snake_size) / 10) * 10
         food_positions.append({"x": food_x, "y": food_y})
     return food_positions
 
 
-Origin_foods = new_food(random.randint(3, 5))
+foods = new_food(random.randint(3, 5))
 
 # 初始贪吃蛇长度
 snakes = []
 length_of_snake = 1
 location_x = WINDOW_WIDTH // 2
 location_y = WINDOW_HEIGHT // 2
+first_snake = {"x": location_x, "y": location_y}
+snakes.append(first_snake)
 
 
 def update_snake_state(request):
@@ -43,8 +45,8 @@ def update_snake_state(request):
     global length_of_snake
     game_data = {
         "game_over": False,
-        "foods": Origin_foods,
-        "snake_list": {"x": location_x, "y": location_y}
+        "foods": foods,
+        "snakes": snakes
     }
 
     # 判断是否出界
@@ -53,8 +55,9 @@ def update_snake_state(request):
         # foods = request.POST.get('foods', None)
         # x1 = request.POST.get('x1', None)
         # y1 = request.POST.get('y1', None)
-        # snake_list = request.POST.get('snake_list', None)
-
+        # snakes = request.POST.get('snakes', None)
+        x1 = WINDOW_WIDTH / 2
+        y1 = WINDOW_HEIGHT / 2
         if x1 >= WINDOW_WIDTH or x1 < 0 or y1 >= WINDOW_HEIGHT or y1 < 0:
             game_data['game_over'] = True
         if key_event == 'ArrowLeft':
@@ -74,41 +77,38 @@ def update_snake_state(request):
 
         # 绘制贪吃蛇，snake_head在列表的队尾
         snake_head = {"x": x1, "y": y1}
-        snake_list.append(snake_head)
+        snakes.append(snake_head)
 
-        for food_x, food_y in foods:
-            if x1 == food_x and y1 == food_y:
-                foods.remove((food_x, food_y))
+        # 吃到食物的逻辑
+        for each_food in foods:
+            if x1 == each_food['x'] and y1 == each_food['y']:
+                foods.remove(each_food)
                 length_of_snake += 1
 
         # 移除蛇尾的块，如果吃到食物，则不需要移除
-        if len(snake_list) > length_of_snake:
-            del snake_list[0]
+        if len(snakes) > length_of_snake:
+            del snakes[0]
 
         # 蛇咬到自己，游戏结束
-        for x in snake_list[:-1]:
+        for x in snakes[:-1]:
             if x == snake_head:
                 game_data['game_over'] = True
-
-        # 吃到食物的逻辑
-        for food_x, food_y in foods:
-            if x1 == food_x and y1 == food_y:
-                foods.remove((food_x, food_y))
-                length_of_snake += 1
 
         if len(foods) < 5:
             foods.extend(new_food(5))
 
         game_data["foods"] = foods
-        game_data["snake_list"] = snake_list
+        game_data["snakes"] = snakes
 
         return JsonResponse(game_data)
 
 
 def snake(request):
+    print(foods)
+    print(snakes)
     return render(request, "snake/snake_v1.html", {"WINDOW_WIDTH": WINDOW_WIDTH,
                                                    "WINDOW_HEIGHT": WINDOW_HEIGHT,
                                                    "move_speed": move_speed,
                                                    "snake_size": snake_size,
-                                                   "Origin_foods": Origin_foods,
+                                                   "Origin_foods": foods,
                                                    "Snakes": snakes})

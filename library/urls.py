a5f678b6773f60422d110dfa9cc0b0590a4bfef8
muzifan1003 <muzifan1003@outlook.com>
2023-09-27 00:55:43 +0800

    modify space_ship

--- library/urls.py
+++ library/urls.py
@@ -109,6 +109,7 @@ urlpatterns = [
     path("update_snake_state/", snake_view.update_snake_state, name='update_snake_state'),
 
     path("spaceship_shoot/", spaceship_shoot_view.spaceship_shoot, name="spaceship_shoot"),
+    path("update_space_ship_state/",spaceship_shoot_view.update_space_ship_state,name='update_space_ship_state'),
     path('users_manage/', user_info_manage_view.users_manage, name='users_manage'),
     path('library_front_page', library_front_page_views.library_front_page, name='library_front_page'),
     path('favicon.ico', RedirectView.as_view(url='/static/ico/favicon.ico'))
--- spaceship_shoot/views.py
+++ spaceship_shoot/views.py
@@ -29,12 +29,12 @@ score = 0
 def generate_enemy(n):
     for _ in range(n):
         enemy_x = random.randint(0, width - image_size)
-        enemy_y = random.randint(50, 100)
+        enemy_y = random.randint(-250, -50)
         enemies_location.append({"loc_x": enemy_x, "loc_y": enemy_y})
 
 
-def generate_bullet():
-    bullet_x = width // 2
+def generate_bullet(_bullet_x):
+    bullet_x = _bullet_x+image_size//2
     bullet_y = height - image_size - bullet_size
     bullets.append({"bullet_x": bullet_x,
                     "bullet_y": bullet_y})
@@ -51,18 +51,21 @@ def update_space_ship_state(request):
         'cur_play_ship_x': play_ship_x
     }
     if request.method == 'POST':
-        print("request.method :", request.POST)
-        key_event = request.POST.get('key_event', None)
-        cur_bullets = request.POST.get('cur_bullets', None)
-        cur_enemies = request.POST.get('cur_enemies', None)
-        cur_play_ship_x = request.POST.get('cur_play_ship_x', None)
+        data_json = request.body.decode('utf-8')
+        data = json.loads(data_json)
 
+        key_event = data.get('key_event', None)
+        cur_bullets = data.get('cur_bullets', None)
+        cur_enemies = data.get('cur_enemies', None)
+        cur_play_ship_x = data.get('cur_play_ship_x', None)
+        print("request.method :", key_event)
         if key_event == 'ArrowLeft':
             cur_play_ship_x -= 10
         elif key_event == 'ArrowRight':
             cur_play_ship_x += 10
         elif key_event == 'ArrowUp':
-            generate_bullet()
+            generate_bullet(cur_play_ship_x)
+            cur_bullets.extend(bullets)
         # 碰撞则 #删除子弹 删除敌机
         for each_bullet in cur_bullets:
             for each_enemy in cur_enemies:
@@ -76,26 +79,30 @@ def update_space_ship_state(request):
         # 敌机到底部则消失
         for each_enemy in cur_enemies:
             each_enemy['loc_y'] += 2
+            print("YY")
             if each_enemy['loc_y'] >= height:
                 cur_enemies.remove(each_enemy)
                 generate_enemy(1)
         # 子弹到顶部则消失
         for each_bullet in cur_bullets:
+            each_bullet['bullet_y']-=2
             if each_bullet['bullet_y'] >= height:
                 cur_bullets.remove(each_bullet)
         # 战机碰撞敌机则游戏结束
         for each_enemy in cur_enemies:
             if (play_ship_x - each_enemy['loc_x'] < image_size or each_enemy['loc_x'] - play_ship_x < image_size) and \
-                    each_enemy['loc_y'] + image_size < play_ship_y:
+                    each_enemy['loc_y'] + image_size >= play_ship_y:
                 game_data['game_over'] = True
         game_data['cur_enemies'] = cur_enemies
         game_data['cur_bullets'] = cur_bullets
         game_data['score'] = score
         game_data['cur_play_ship_x'] = cur_play_ship_x
+        print("game_data:",game_data)
         return JsonResponse(game_data)
 
 
 def spaceship_shoot(request):
+    generate_enemy(5)
     context = {'play_ship_x': play_ship_x,
                'play_ship_y': play_ship_y,
                'play_ship_img': json.dumps(play_ship_img),

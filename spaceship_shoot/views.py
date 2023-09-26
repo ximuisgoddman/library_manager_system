8fb3b108c0821ac5fa32f18110614d431838c5a1
ximuisgoddman <72920822+ximuisgoddman@users.noreply.github.com>
2023-09-26 20:48:34 +0800

    Update views.py

--- spaceship_shoot/views.py
+++ spaceship_shoot/views.py
@@ -3,151 +3,107 @@ import random
 import os
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 
-base_dir = os.path.dirname(os.path.dirname(__file__))
 # 加载玩家飞船图片
-player_ship = pygame.image.load(os.path.join(base_dir, "my_resource/game_info/game_image/汤姆猫.jpg"))
-player_ship = pygame.transform.scale(player_ship, (50, 50))
+play_ship_img = "game_info/game_image/汤姆猫.jpg"
 
-#
 # 设置游戏窗口尺寸
 width = 800
 height = 600
-# 加载玩家飞船图片
-player_rect = player_ship.get_rect()
-player_rect.centerx = width // 2
-player_rect.bottom = height - 10
 
+image_size = 50
+# 加载玩家飞船图片
+play_ship_x = width // 2 - image_size // 2
+play_ship_y = height - image_size
 # 加载敌人飞船图片
-enemy_ship = pygame.image.load(os.path.join(base_dir, "my_resource/game_info/game_image/杰利鼠.png"))
-enemy_ship = pygame.transform.scale(enemy_ship, (50, 50))
-enemies = []
-for _ in range(5):
-    enemy = enemy_ship.get_rect()
-    enemy.x = random.randint(0, width - enemy.width)
-    enemy.y = random.randint(-200, -50)
-    enemies.append(enemy)
-#
+enemy_ship_img = "game_info/game_image/杰利鼠.png"
+enemies_location = []
+
 # 定义子弹列表
 bullets = []
+bullet_size = 4
+score = 0
 
 
-# running = True
-# while running:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
-#         elif event.type == pygame.KEYDOWN:
-#             if event.key == pygame.K_SPACE:
-#                 bullets.append(pygame.Rect(player_rect.centerx - 2, player_rect.top, 4, 10))  # 创建一个子弹矩形
-#
-#     # 移动玩家飞船
-#     keys = pygame.key.get_pressed()
-#     if keys[pygame.K_LEFT]:
-#         player_rect.move_ip(-5, 0)
-#     if keys[pygame.K_RIGHT]:
-#         player_rect.move_ip(5, 0)
-#
-#     # 移动子弹
-#     for bullet in bullets:
-#         bullet.move_ip(0, -10)
-#         if bullet.bottom <= 0:
-#             bullets.remove(bullet)
-#
-#     # 移动敌人
-#     for enemy in enemies:
-#         enemy.y += 2
-#         if enemy.y > height:
-#             enemy.y = random.randint(-200, -50)
-#             enemy.x = random.randint(0, width - enemy.width)
-#
-#     # 检测碰撞
-#     for enemy in enemies:
-#         if player_rect.colliderect(enemy):
-#             running = False
-#         for bullet in bullets:
-#             if bullet.colliderect(enemy):
-#                 bullets.remove(bullet)
-#                 enemy.y = random.randint(-200, -50)
-#                 enemy.x = random.randint(0, width - enemy.width)
-#                 score += 10
-#
-#     # 绘制背景
-#     screen.fill((0, 0, 0))
-#
-#     # 绘制玩家飞船
-#     screen.blit(player_ship, player_rect)
-#
-#     # 绘制敌人
-#     for enemy in enemies:
-#         screen.blit(enemy_ship, enemy)
-#
-#     # 绘制子弹
-#     for bullet in bullets:
-#         pygame.draw.rect(screen, (255, 255, 255), bullet)
-#
-#     # 绘制得分
-#     score_text = font.render("Score: " + str(score), True, (255, 255, 255))
-#     screen.blit(score_text, (10, 10))
-#
-#     pygame.display.flip()
-#     clock.tick(60)
-#
-# # 退出游戏
-# pygame.quit()
+def generate_enemy(n):
+    for _ in range(n):
+        enemy_x = random.randint(0, width - image_size)
+        enemy_y = random.randint(50, 100)
+        enemies_location.append({"loc_x": enemy_x, "loc_y": enemy_y})
+
+
+def generate_bullet():
+    bullet_x = width // 2
+    bullet_y = height - image_size - bullet_size
+    bullets.append({"bullet_x": bullet_x,
+                    "bullet_y": bullet_y})
 
 
 # 修改 update_space_ship_state 函数
 def update_space_ship_state(request):
-    global player_rect, enemies, bullets, score
-
-    # 处理移动玩家飞船
-    keys = request.GET.get('keys', '')
-    if 'left' in keys:
-        player_rect.move_ip(-5, 0)
-    if 'right' in keys:
-        player_rect.move_ip(5, 0)
-    if "backend" in keys:
-        bullets.append(pygame.Rect(player_rect.centerx - 2, player_rect.top, 4, 10))  # 创建一个子弹矩形
-
-    # 移动子弹
-    for bullet in bullets:
-        bullet.move_ip(0, -10)
-        if bullet.bottom <= 0:
-            bullets.remove(bullet)
-
-    # 移动敌人
-    for enemy in enemies:
-        enemy.y += 2
-        if enemy.y > height:
-            enemy.y = random.randint(-200, -50)
-            enemy.x = random.randint(0, width - enemy.width)
-
-    # 检测碰撞
-    for enemy in enemies:
-        if player_rect.colliderect(enemy):
-            # 游戏结束
-            game_data = {"game_over": True}
-            return JsonResponse(game_data)
-
-        for bullet in bullets:
-            if bullet.colliderect(enemy):
-                bullets.remove(bullet)
-                enemy.y = random.randint(-200, -50)
-                enemy.x = random.randint(0, width - enemy.width)
-                score += 10
-
-    # 游戏未结束，返回游戏数据
+    global score
     game_data = {
-        "game_over": False,
-        "player_x": player_rect.x,
-        "enemies": [{"x": enemy.x, "y": enemy.y} for enemy in enemies],
-        "bullets": [{"x": bullet.x, "y": bullet.y} for bullet in bullets],
-        "score": score
+        'game_over': False,
+        'cur_bullets': bullets,
+        'score': score,
+        'cur_enemies': enemies_location,
+        'cur_play_ship_x': play_ship_x
     }
+    if request.method == 'POST':
+        print("request.method :", request.POST)
+        key_event = request.POST.get('key_event', None)
+        cur_bullets = request.POST.get('cur_bullets', None)
+        cur_enemies = request.POST.get('cur_enemies', None)
+        cur_play_ship_x = request.POST.get('cur_play_ship_x', None)
 
-    return JsonResponse(game_data)
+        if key_event == 'ArrowLeft':
+            cur_play_ship_x -= 10
+        elif key_event == 'ArrowRight':
+            cur_play_ship_x += 10
+        elif key_event == 'ArrowUp':
+            generate_bullet()
+        # 碰撞则 #删除子弹 删除敌机
+        for each_bullet in cur_bullets:
+            for each_enemy in cur_enemies:
+                if each_bullet['bullet_x'] >= each_enemy['loc_x'] \
+                        and each_bullet['bullet_x'] <= each_enemy['loc_x'] + image_size \
+                        and each_bullet['bullet_y'] <= each_enemy['loc_y'] + image_size:
+                    cur_bullets.remove(each_bullet)
+                    generate_enemy(1)
+                    score += 1
+
+        # 敌机到底部则消失
+        for each_enemy in cur_enemies:
+            each_enemy['loc_y'] += 2
+            if each_enemy['loc_y'] >= height:
+                cur_enemies.remove(each_enemy)
+                generate_enemy(1)
+        # 子弹到顶部则消失
+        for each_bullet in cur_bullets:
+            if each_bullet['bullet_y'] >= height:
+                cur_bullets.remove(each_bullet)
+        # 战机碰撞敌机则游戏结束
+        for each_enemy in cur_enemies:
+            if (play_ship_x - each_enemy['loc_x'] < image_size or each_enemy['loc_x'] - play_ship_x < image_size) and \
+                    each_enemy['loc_y'] + image_size < play_ship_y:
+                game_data['game_over'] = True
+        game_data['cur_enemies'] = cur_enemies
+        game_data['cur_bullets'] = cur_bullets
+        game_data['score'] = score
+        game_data['cur_play_ship_x'] = cur_play_ship_x
+        return JsonResponse(game_data)
 
 
 def spaceship_shoot(request):
-    return render(request, "spaceship_shoot/spaceship_shoot_v1.html")
+    context = {'play_ship_x': play_ship_x,
+               'play_ship_y': play_ship_y,
+               'play_ship_img': json.dumps(play_ship_img),
+               'enemies_location': json.dumps(enemies_location),
+               'enemy_ship_img': json.dumps(enemy_ship_img),
+               'score': 0,
+               "bullets": json.dumps(bullets),
+               'image_size': image_size,
+               'bullet_size': bullet_size}
+    print(context)
+    return render(request, "spaceship_shoot/spaceship_shoot_v1.html", context)

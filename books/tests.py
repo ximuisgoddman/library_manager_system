186f78802aa77086d83f20be60128b115764c3df
ximuisgoddman <72920822+ximuisgoddman@users.noreply.github.com>
2023-11-24 22:45:18 +0800

    modify

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+with open('D:\my_program\py\library_manager_system/requirements.txt','r',encoding='utf-8') as fr:
+    tmp=fr.readlines()
+
+target=[]
+for x in tmp:
+    target.append(x.split(" ")[0]+"=="+x.split(" ")[-1])
+
+with open('D:\my_program\py\library_manager_system/requirements.txt','w',encoding='utf-8') as fw:
+    for x in target:
+        fw.write(x)

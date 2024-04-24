97a8b376c7d90616083069249b1cbd44987f848e
ximuisgoddman <72920822+ximuisgoddman@users.noreply.github.com>
2024-04-24 20:08:30 +0800

    Update tasks.py

--- article/tasks.py
+++ article/tasks.py
@@ -6,9 +6,12 @@ from django.core.cache import cache
 
 
 @shared_task
-def update_article_likes():
+def update_article_likes_collect():
     all_article_ids = ArticlePost.objects.values('id')
     for each_id in all_article_ids:
         _likes = cache.get("article_%s_likes" % each_id['id'])
         if _likes:
             ArticlePost.objects.filter(id=each_id['id']).update(likes=_likes)
+        _collect = cache.get("article_%s_collect" % each_id['id'])
+        if _collect:
+            ArticlePost.objects.filter(id=each_id['id']).update(collects=_collect)
